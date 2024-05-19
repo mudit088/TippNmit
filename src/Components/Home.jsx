9908ef9baa7643ad01f833ae90c120615ebe8ef5
mudit088 <muditsri25@gmail.com>
2024-05-20 00:49:35 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
         <p className='text-3xl text-blue-800 font-semibold mr-56 pb-48'>No <span className='font-semibold'>App</span>  needed, Just your <span className='text-teal-400 font-bold'>Phone</span> </p>
       </div>
 
-      <div className='flex  items-center'>
+      <div className='flex  items-center mb-28'>
         <p className='text-3xl text-blue-800 ml-56 font-semibold'>Fast & simple cashless tipping for <span className='text-teal-400 font-bold'>Hotels</span> <br /> <span className='text-xl'>Attract, retain, and motivate your hardworking <br /> staff while delighting your guests.</span> </p>
         
         <img src="/phone.jpg" className='h-96 ml-48 hover:scale-110 duration-300' alt="" />
@@ -104,7 +104,7 @@ const Home = () => {
        {/* //hotelgrid */}
 
       <section className='m-5 bg-slate-100  rounded-xl p-5'>
-      <div className='mt-10 mb-10 flex flex-col justify-center items-center'>
+      <div className='mt-2 mb-10 flex flex-col justify-center items-center'>
           <p className='text-3xl text-blue-800 font-semibold '>Below are popular examples of where to display your <span className='text-teal-400 font-bold'>QR codes:</span> </p>
 
                  <div className='grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
@@ -157,10 +157,71 @@ const Home = () => {
 
           {/* gridend */}
 
+          <div className='flex flex-col justify-center items-center  mt-36'>
+          <p className='text-4xl text-blue-800 font-semibold'>Features for <span className='text-teal-400 font-bold '>Restaurant</span>  Teams.</p>
+              
+              <div className='grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
+
+              <div className='pt-10 hover:scale-110 duration-300'>
+            <img src="/res1.svg" className='h-28 ml-10 ' alt="" />
+            <p className='text-blue-700 font-semibold text-xl'>Team tips are safe & <br /> secure with eTip. <br /> <span className='text-xs text-blue-500'>No more worrying about misplacing <br /> physical cash tips. Tips are securely <br /> transferred to team members.</span> </p>
+          </div>
+
+
+          <div className='pt-10 hover:scale-110 duration-300'>
+            <img src="/res2.svg" className='h-24 ml-14 pb-2 ' alt="" />
+            <p className='text-blue-700 font-semibold text-xl pt-2'>Save time and remove the <br /> headache of cash tips <br /> <span className='text-xs text-blue-500'>There's no legwork required for your team <br /> to receive their tips. Payments are paid <br /> directly to their existing bank accounts.</span> </p>
+          </div>
+
+          <div className='pt-10 hover:scale-110 duration-300'>
+            <img src="/res3.svg" className='h-24 ml-14 pb-2 ' alt="" />
+            <p className='text-blue-700 font-semibold text-xl pt-2'>Improve team financial <br /> wellness <br /> <span className='text-xs text-blue-500'>Removing delays between tip payouts<br /> allows your team to manage their money <br /> with ease, planning ahead.</span> </p>
+          </div>
+
+
+              </div>
+              </div>
+
+
+              <section className='m-5 bg-slate-100  rounded-xl p-5'>
+      <div className='mt-2 mb-10 flex flex-col justify-center items-center'>
+          <p className='text-3xl text-blue-800 font-semibold '>Digital tip jars for your restaurant </p>
+
+                 <div className='grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
+            
+          <div className='shadow-md shadow-gray-500 rounded-md duration-200 hover:scale-105'>
+              <img className='rounded-md duration-200 hover:scale-105 h-56' src="/resj1.jpg" alt="" />
+              <div className='flex items-center justify-center'>
+                 <p className='text-teal-400 font-semibold py-4'>At your order counters.</p>
+              </div>
+          </div>
+
+          <div className='shadow-md shadow-gray-500 rounded-md duration-200 hover:scale-105'>
+              <img className='rounded-md duration-200 hover:scale-105 h-56' src="/resj2.jpg" alt="" />
+              <div className='flex items-center justify-center'>
+                 <p className='text-teal-400 font-semibold py-4'>On each of your tables</p>
+              </div>
+          </div>
+
+          
+          <div className='shadow-md shadow-gray-500 rounded-md duration-200 hover:scale-105'>
+              <img className='rounded-md duration-200 hover:scale-105 h-56' src="/resj3.jpg" alt="" />
+              <div className='flex items-center justify-center'>
+                 <p className='text-teal-400 font-semibold py-4'>Added to your deliveries</p>
+              </div>
+          </div>
+          </div>
+
+         
 
           
 
-      </div>
+
+          </div>
+          </section>
+           </div>
+
+      
       
 
     
